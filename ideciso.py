3b98c0e9a3305f96501742c168894cda9d6b9b9f
Gabriela Boriero <gabrielamb2@al.inper.edu.br>
2020-03-23 13:06:55 -0300

    .

---
+++ ideciso.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import rospy
+import numpy as np
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import LaserScan
+
+
+medida = -1
+
+def scaneou(dado):
+	print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+	print("Leituras:")
+	global medida
+	medida = np.array(dado.ranges).round(decimals=2)[0]
+	print(medida)
+	# print("Intensities")
+	# print(np.array(dado.intensities).rounddecimals(=2))
+
+
+
+if __name__=="__main__":
+
+	rospy.init_node("le_scan")
+
+	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+	
+
+
+	while not rospy.is_shutdown():
+		if medida < 0.98:
+			velocidade = Twist(Vector3(-0.5, 0, 0), Vector3(0, 0, 0))
+			velocidade_saida.publish(velocidade)
+			rospy.sleep(0.25)
+		elif medida > 1.02:
+			velocidade = Twist(Vector3(0.5, 0, 0), Vector3(0, 0, 0))
+			velocidade_saida.publish(velocidade)
+			rospy.sleep(0.25)
+
+
---
+++ roda_meu.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist, Vector3
+
+v = 0.18  # Velocidade linear
+v_2 = 0.09
+w = 0.27  # Velocidade angular
+
+if __name__ == "__main__":
+    rospy.init_node("roda_exemplo")
+    pub = rospy.Publisher("cmd_vel", Twist, queue_size=3)
+
+    try:
+        while not rospy.is_shutdown():
+	    vel_2 = Twist(Vector3(0,0,0), Vector3(0,0,w))
+	    pub.publish(vel_2)
+	    rospy.sleep(6)
+	    vel_1 = Twist(Vector3(0,0,0), Vector3(0,0,0))
+	    pub.publish(vel_1)
+	    rospy.sleep(2.0)
+	    vel_4 = Twist(Vector3(v_2,0,0), Vector3(0,0,0))
+            pub.publish(vel_4)
+	    rospy.sleep(4.0)
+            vel = Twist(Vector3(v,0,0), Vector3(0,0,0))
+            pub.publish(vel)
+	    rospy.sleep(3.0)
+	    vel_3 = Twist(Vector3(v_2,0,0), Vector3(0,0,0))
+            pub.publish(vel_3)
+	    rospy.sleep(4.0)
+	    
+	    
+
+    except rospy.ROSInterruptException:
+        print("Ocorreu uma exceção com o rospy")
