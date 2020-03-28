cd67dab7f045255c266c31eb9ddb52ffdb0569c1
Fernando_Fincatti <fernandocfbf@al.insper.edu.br>
2020-03-28 17:05:02 -0300

    Consegui fazer o robo andar

--- projeto_ros/scripts/cor.py
+++ projeto_ros/scripts/cor.py
@@ -28,9 +28,8 @@ from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import LaserScan
 import cormodule
-from le_scan import*
 
-medida = -1
+maior_area = 0
 
 bridge = CvBridge()
 
@@ -45,24 +44,21 @@ area = 0.0 # Variavel com a area do maior contorno
 # Descarta imagens que chegam atrasadas demais
 check_delay = False 
 
-#############################################################################
-
-
+############################
+medida = -1
 
 def scaneou(dado):
-	print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
-	print("Leituras:")
+	#print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+	#print("Leituras:")
 	global medida
 	medida = np.array(dado.ranges).round(decimals=2)[0]
-	print(medida)
-
 	#print("Intensities")
 	#print(np.array(dado.intensities).round(decimals=2))
-#############################################################################
+############################
 
 # A função a seguir é chamada sempre que chega um novo frame
 def roda_todo_frame(imagem):
-	print("frame")
+	#print("frame")
 	global cv_image
 	global media
 	global centro
@@ -71,14 +67,15 @@ def roda_todo_frame(imagem):
 	imgtime = imagem.header.stamp
 	lag = now-imgtime # calcula o lag
 	delay = lag.nsecs
-	print("delay ", "{:.3f}".format(delay/1.0E9))
+	#print("delay ", "{:.3f}".format(delay/1.0E9))
 	if delay > atraso and check_delay==True:
-		print("Descartando por causa do delay do frame:", delay)
+		#print("Descartando por causa do delay do frame:", delay)
 		return 
 	try:
 		antes = time.clock()
 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
 		# cv_image = cv2.flip(cv_image, -1)
+		global maior_area
 		media, centro, maior_area =  cormodule.identifica_cor(cv_image)
 		depois = time.clock()
 		cv2.imshow("Camera", cv_image)
@@ -87,7 +84,6 @@ def roda_todo_frame(imagem):
 	
 if __name__=="__main__":
 	rospy.init_node("cor")
-
 
 	# topico_imagem = "/kamera"
 	topico_imagem = "/camera/rgb/image_raw/compressed"
@@ -112,37 +108,54 @@ if __name__=="__main__":
 	# 
 
 	recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
+
 	print("Usando ", topico_imagem)
 
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 	try:
 
 		while not rospy.is_shutdown():
+			#print(maior_area)
+			print("medida: ",medida)
 			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+
 			if len(media) != 0 and len(centro) != 0:
-				print("Média dos vermelhos: {0}, {1}".format(media[0], media[1]))
-				print("Centro dos vermelhos: {0}, {1}".format(centro[0], centro[1]))
+				#print("Média dos vermelhos: {0}, {1}".format(media[0], media[1]))
+				#print("Centro dos vermelhos: {0}, {1}".format(centro[0], centro[1]))
+				diferenca = media[0] - centro[0] 
+				print("diferença: ",diferenca)
 
-				if (media[0] > centro[0]):
-					vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.1))
-					velocidade_saida.publish(vel)
-					rospy.sleep(0.1)
+				if maior_area <= 3500:
+
+					if media[0] > centro[0]:
+						vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.01))
+						velocidade_saida.publish(vel)
+						rospy.sleep(0.1)
+
+				elif maior_area > 3500:
 					
-				if (media[0] < centro[0]):
-					vel = Twist(Vector3(0,0,0), Vector3(0,0,0.1))
-					velocidade_saida.publish(vel)
-					rospy.sleep(0.1)
+					if media[0] < centro[0]:
+						vel = Twist(Vector3(0,0,0), Vector3(0,0,0.1))
+						velocidade_saida.publish(vel)
+						rospy.sleep(0.1)
 
-				if media[0] - centro[0] < 5:
-					if medida < 0.20:
-						velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
-						velocidade_saida.publish(velocidade)
-						rospy.sleep(0.55)
+					if media[0] > centro[0]:
+						vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.1))
+						velocidade_saida.publish(vel)
+						rospy.sleep(0.1)
+
+					if diferenca <= 10:
+
+						if medida < 0.20:
+							velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+							velocidade_saida.publish(velocidade)
+							rospy.sleep(0.55)
+								
+						elif medida > 1.75:
+							velocidade = Twist(Vector3(0.1, 0, 0), Vector3(0, 0, 0))
+							velocidade_saida.publish(velocidade)
+							rospy.sleep(0.55)
 						
-					elif medida > 1.75:
-						velocidade = Twist(Vector3(0.5, 0, 0), Vector3(0, 0, 0))
-						velocidade_saida.publish(velocidade)
-						rospy.sleep(0.55)
-				
 	except rospy.ROSInterruptException:
 	    print("Ocorreu uma exceção com o rospy")
--- projeto_ros/scripts/le_scan.py
+++ projeto_ros/scripts/le_scan.py
@@ -8,17 +8,15 @@ import numpy as np
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 
+medida = -1
 
 def scaneou(dado):
 	print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
 	print("Leituras:")
 	global medida
 	medida = np.array(dado.ranges).round(decimals=2)[0]
-	print(medida)
 	#print("Intensities")
 	#print(np.array(dado.intensities).round(decimals=2))
-
-	
 
 
 if __name__=="__main__":
@@ -30,14 +28,14 @@ if __name__=="__main__":
 
 
 
-	while not rospy.is_shutdown():
-		if medida < 0.20:
-			velocidade = Twist(Vector3(-0.5, 0, 0), Vector3(0, 0, 0))
-			velocidade_saida.publish(velocidade)
-			rospy.sleep(0.25)
-		elif medida > 1.75:
-			velocidade = Twist(Vector3(0.5, 0, 0), Vector3(0, 0, 0))
-			velocidade_saida.publish(velocidade)
-			rospy.sleep(0.25)
+#	while not rospy.is_shutdown():
+#		if medida < 0.20:
+#			velocidade = Twist(Vector3(-0.5, 0, 0), Vector3(0, 0, 0))
+#			velocidade_saida.publish(velocidade)
+#			rospy.sleep(0.25)
+#		elif medida > 1.75:
+#			velocidade = Twist(Vector3(0.5, 0, 0), Vector3(0, 0, 0))
+#			velocidade_saida.publish(velocidade)
+#			rospy.sleep(0.25)
 
 
